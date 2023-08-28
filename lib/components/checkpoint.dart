a859518d2b2090b2b91fa884498845b67a56e9ef
Enzo743 <53581150+Enzo743@users.noreply.github.com>
2023-08-28 15:36:23 +0200

    🚧 Add collision to the checkpoint

--- lib/components/checkpoint.dart
+++ lib/components/checkpoint.dart
@@ -1,14 +1,21 @@
 import 'dart:async';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 class Checkpoint extends SpriteAnimationComponent
-    with HasGameRef<PixelAdventure> {
+    with HasGameRef<PixelAdventure>, CollisionCallbacks {
   Checkpoint({position, size}) : super(position: position, size: size);
 
   @override
   FutureOr<void> onLoad() {
+    add(RectangleHitbox(
+      position: Vector2(18, 56),
+      size: Vector2(12, 8),
+      collisionType: CollisionType.passive,
+    ));
+
     animation = SpriteAnimation.fromFrameData(
       game.images
           .fromCache('Items/Checkpoints/Checkpoint/Checkpoint (No Flag).png'),
@@ -20,5 +27,11 @@ class Checkpoint extends SpriteAnimationComponent
     );
 
     return super.onLoad();
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    // TODO: implement onCollision
+    super.onCollision(intersectionPoints, other);
   }
 }
